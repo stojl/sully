a2947d27e2c40cf962f8020f48c3142770c57932
stojl <lucashillerod@hotmail.com>
2024-03-23 18:42:36 +0100

    Removing latrix

--- tests/testthat/test-latrix.R
+++
@@ -1,5 +0,0 @@
-test_that("latrix produces a list with empty data argument", {
-  testthat::expect_type(latrix(ncol = 2, nrow = 2), "list")
-  testthat::expect_type(latrix(list()), "list")
-  testthat::expect_type(latrix(), "list")
-})
